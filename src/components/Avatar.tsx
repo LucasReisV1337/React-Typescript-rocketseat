1de36bfa658b25ed3d1573c2e6373659c21a7f9e
LucasReisV1337 <lucasreisvasconcelos@icloud.com>
2022-12-27 13:06:21 -0300

    h

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,17 @@
+import { ImgHTMLAttributes } from "react";
 import styles from './Avatar.module.css'
 
-export function Avatar({ hasBorder = true, src }) {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
+  hasBorder?: boolean;
+  src: string;
+  alt?: string;
+}
+
+export function Avatar({ hasBorder = true, ...props}: AvatarProps) {
   return (
-    <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src}/>
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      {...props}
+    />
   );
 }
